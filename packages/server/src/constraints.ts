81be3dc7291723185aa265af37b5b0d4660e5287
dholms <dtholmgren@gmail.com>
2023-09-19 10:39:13 -0500

    tidy

--- packages/server/src/constraints.ts
+++ packages/server/src/constraints.ts
@@ -3,12 +3,24 @@ import * as plc from '@did-plc/lib'
 import { ServerError } from './error'
 import { parseDidKey } from '@atproto/crypto'
 
+const MAX_OP_BYTES = 4000
+const MAX_AKA_ENTRIES = 10
+const MAX_AKA_LENGTH = 256
+const MAX_ROTATION_ENTRIES = 10
+const MAX_SERVICE_ENTRIES = 10
+const MAX_SERVICE_TYPE_LENGTH = 256
+const MAX_SERVICE_ENDPOINT_LENGTH = 512
+const MAX_ID_LENGTH = 32
+
 export function assertValidIncomingOp(
   op: unknown,
 ): asserts op is plc.OpOrTombstone {
   const byteLength = cborEncode(op).byteLength
-  if (byteLength > 4000) {
-    throw new ServerError(400, 'Operation too large')
+  if (byteLength > MAX_OP_BYTES) {
+    throw new ServerError(
+      400,
+      `Operation too large (${MAX_OP_BYTES} bytes maximum in cbor encoding)`,
+    )
   }
   if (!check.is(op, plc.def.opOrTombstone)) {
     throw new ServerError(400, `Not a valid operation: ${JSON.stringify(op)}`)
@@ -16,22 +28,31 @@ export function assertValidIncomingOp(
   if (op.type === 'plc_tombstone') {
     return
   }
-  if (op.alsoKnownAs.length > 10) {
-    throw new ServerError(400, 'To many alsoKnownAs items (max 10)')
+  if (op.alsoKnownAs.length > MAX_AKA_ENTRIES) {
+    throw new ServerError(
+      400,
+      `To many alsoKnownAs entries (max ${MAX_AKA_ENTRIES})`,
+    )
   }
   const akaDupe: Record<string, boolean> = {}
   for (const aka of op.alsoKnownAs) {
-    if (aka.length > 256) {
-      throw new ServerError(400, `alsoKnownAs field too long (max 256): ${aka}`)
+    if (aka.length > MAX_AKA_LENGTH) {
+      throw new ServerError(
+        400,
+        `alsoKnownAs entry too long (max ${MAX_AKA_LENGTH}): ${aka}`,
+      )
     }
     if (akaDupe[aka]) {
-      throw new ServerError(400, `duplicate alsoKnownAs field: ${aka}`)
+      throw new ServerError(400, `duplicate alsoKnownAs entry: ${aka}`)
     } else {
       akaDupe[aka] = true
     }
   }
-  if (op.rotationKeys.length > 5) {
-    throw new ServerError(400, 'To many rotationKey items (max 5)')
+  if (op.rotationKeys.length > MAX_ROTATION_ENTRIES) {
+    throw new ServerError(
+      400,
+      `Too many rotationKey entries (max ${MAX_ROTATION_ENTRIES})`,
+    )
   }
   for (const key of op.rotationKeys) {
     try {
@@ -41,26 +62,38 @@ export function assertValidIncomingOp(
     }
   }
   const serviceEntries = Object.entries(op.services)
-  if (serviceEntries.length > 10) {
-    throw new ServerError(400, 'To many service entries (max 10)')
+  if (serviceEntries.length > MAX_SERVICE_ENTRIES) {
+    throw new ServerError(
+      400,
+      `To many service entries (max ${MAX_SERVICE_ENTRIES})`,
+    )
   }
   for (const [id, service] of serviceEntries) {
-    if (id.length > 32) {
-      throw new ServerError(400, `Service id too long (max 32): ${id}`)
+    if (id.length > MAX_ID_LENGTH) {
+      throw new ServerError(
+        400,
+        `Service id too long (max ${MAX_ID_LENGTH}): ${id}`,
+      )
     }
-    if (service.type.length > 256) {
-      throw new ServerError(400, 'Service type too long (max 256)')
+    if (service.type.length > MAX_SERVICE_TYPE_LENGTH) {
+      throw new ServerError(
+        400,
+        `Service type too long (max ${MAX_SERVICE_TYPE_LENGTH})`,
+      )
     }
-    if (service.endpoint.length > 512) {
-      throw new ServerError(400, 'Service endpoint too long (max 512)')
+    if (service.endpoint.length > MAX_SERVICE_ENDPOINT_LENGTH) {
+      throw new ServerError(
+        400,
+        `Service endpoint too long (max ${MAX_SERVICE_ENDPOINT_LENGTH})`,
+      )
     }
   }
   const verifyMethods = Object.entries(op.verificationMethods)
   for (const [id, key] of verifyMethods) {
-    if (id.length > 32) {
+    if (id.length > MAX_ID_LENGTH) {
       throw new ServerError(
         400,
-        `Verification Method id too long (max 32): ${id}`,
+        `Verification Method id too long (max ${MAX_ID_LENGTH}): ${id}`,
       )
     }
     try {
