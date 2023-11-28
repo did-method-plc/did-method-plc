cc35e5384619859e0625ed769aa818c51f8e4605
dholms <dtholmgren@gmail.com>
2023-11-28 14:44:26 -0600

    rename

--- packages/server/src/migrations/20231128T203323431Z-did-createdat-idx.ts
+++ packages/server/src/migrations/20231128T203323431Z-did-createdat-idx.ts
@@ -2,12 +2,12 @@ import { Kysely } from 'kysely'
 
 export async function up(db: Kysely<unknown>): Promise<void> {
   await db.schema
-    .createIndex('operations_did_createdAt_index')
+    .createIndex('operations_did_createdat_idx')
     .on('operations')
     .columns(['did', 'createdAt'])
     .execute()
 }
 
 export async function down(db: Kysely<unknown>): Promise<void> {
-  await db.schema.dropIndex('operations_did_createdAt_index').execute()
+  await db.schema.dropIndex('operations_did_createdat_idx').execute()
 }
