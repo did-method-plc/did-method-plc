332eda18f704b66faabe59f016e9f455bb8eec5c
David Buchanan <david@blueskyweb.xyz>
2025-12-01 16:00:12 +0000

    leader service entrypoint

---
+++ packages/server/service/leader.js
@@ -0,0 +1,39 @@
+'use strict' /* eslint-disable */
+
+require('dd-trace/init') // Only works with commonjs
+const { Database, SequencerLeader } = require('..')
+
+const main = async () => {
+  const dbCreds = JSON.parse(process.env.DB_CREDS_JSON)
+  const dbSchema = process.env.DB_SCHEMA || undefined
+  const dbPoolSize = parseMaybeInt(process.env.DB_POOL_SIZE)
+  const dbPoolMaxUses = parseMaybeInt(process.env.DB_POOL_MAX_USES)
+  const dbPoolIdleTimeoutMs = parseMaybeInt(process.env.DB_POOL_IDLE_TIMEOUT_MS)
+  const db = Database.postgres({
+    url: pgUrl(dbCreds),
+    schema: dbSchema,
+    poolSize: dbPoolSize,
+    poolMaxUses: dbPoolMaxUses,
+    poolIdleTimeoutMs: dbPoolIdleTimeoutMs,
+  })
+  const leader = new SequencerLeader(db)
+  leader.run().catch((err) => {
+    console.error('Sequencer leader error:', err)
+  })
+
+  // Graceful shutdown (see also https://aws.amazon.com/blogs/containers/graceful-shutdowns-with-ecs/)
+  process.on('SIGTERM', async () => {
+    await leader.destroy()
+  })
+}
+
+const pgUrl = ({ username = "postgres", password = "postgres", host = "localhost", port = "5432", database = "postgres", sslmode }) => {
+  const enc = encodeURIComponent
+  return `postgresql://${username}:${enc(password)}@${host}:${port}/${database}${sslmode ? `?sslmode=${enc(sslmode)}` : ''}`
+}
+
+const parseMaybeInt = (str) => {
+  return str ? parseInt(str, 10) : undefined
+}
+
+main()
